1112c26a1d9e3f037f730d6148eb4d08cd8e8235
Manoj Gautam <surfer.manoj@gmail.com>
2015-11-20 17:28:46 +0545

    wsgi

--- trydjango18/wsgi.py
+++ trydjango18/wsgi.py
@@ -10,14 +10,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 # from django.conf import settings
- from django.core.wsgi import get_wsgi_application
-
- os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango18.settings")
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango18.settings")
 # application = get_wsgi_application()
-
-
-
 from dj_static import Cling
-
 application = Cling(get_wsgi_application())
 
