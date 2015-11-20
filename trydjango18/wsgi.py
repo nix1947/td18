9feaeb59010ec1ad0b0649c3a7cb27909766ad79
Manoj Gautam <surfer.manoj@gmail.com>
2015-11-20 17:21:38 +0545

    wsgi changed

--- trydjango18/wsgi.py
+++ trydjango18/wsgi.py
@@ -8,20 +8,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 
-# import os
+import os
 # from django.conf import settings
-# from django.core.wsgi import get_wsgi_application
+ from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango18.settings")
+ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trydjango18.settings")
 # application = get_wsgi_application()
 
-# if not settings.DEBUG:
-# 	from dj_static import Cling
-# 	application = Cling(get_wsgi_application())
 
 
-
-from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
